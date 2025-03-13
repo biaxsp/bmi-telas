ab209fa5030b673910f3f2f9d9267a244ee9fd12
Prof Celso <celso.furtado@sp.senai.br>
2025-03-13 17:21:07 -0300

    Tela de dados concluída

---
+++ app/src/main/java/br/senai/sp/jandira/bmi_ds2aita/screens/UserDataScreen.kt
@@ -0,0 +1,252 @@
+package br.senai.sp.jandira.bmi_ds2aita.screens
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Balance
+import androidx.compose.material.icons.filled.Height
+import androidx.compose.material.icons.filled.Numbers
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.bmi_ds2aita.R
+
+@Composable
+fun UserDataScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(
+                    listOf(
+                        Color(0xFF9C27B0),
+                        Color(0xFF3F51B5),
+                    )
+                )
+            )
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Text(
+                text = stringResource(R.string.hi),
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(32.dp)
+                    .weight(1f)
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(4f),
+                colors = CardDefaults
+                    .cardColors(
+                        containerColor = Color.White
+                    )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp),
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .weight(1f),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Card(
+                                modifier = Modifier
+                                    .size(130.dp),
+                                shape = CircleShape,
+                                border = BorderStroke(
+                                    width = 4.dp,
+                                    brush = Brush.linearGradient(
+                                        listOf(
+                                            Color(0xFF673AB7),
+                                            Color(0xFF9C27B0),
+                                        )
+                                    )
+                                )
+                            ) {
+                                Image(
+                                    painter = painterResource(R.drawable.boy),
+                                    contentDescription = ""
+                                )
+                            }
+                            Button(
+                                onClick = {},
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp),
+                                colors = ButtonDefaults
+                                    .buttonColors(
+                                        containerColor = Color(0xFF2196F3)
+                                    )
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.male)
+                                )
+                            }
+                        }
+                        Column(
+                            modifier = Modifier
+                                .weight(1f),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Card(
+                                modifier = Modifier
+                                    .size(130.dp),
+                                shape = CircleShape,
+                                border = BorderStroke(
+                                    width = 4.dp,
+                                    brush = Brush.linearGradient(
+                                        listOf(
+                                            Color(0xFF673AB7),
+                                            Color(0xFF9C27B0),
+                                        )
+                                    )
+                                )
+                            ) {
+                                Image(
+                                    painter = painterResource(R.drawable.girl),
+                                    contentDescription = ""
+                                )
+                            }
+                            Button(
+                                onClick = {},
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp),
+                                colors = ButtonDefaults
+                                    .buttonColors(
+                                        containerColor = Color(0xFF9C27B0)
+                                    )
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.female)
+                                )
+                            }
+                        }
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            value = "",
+                            onValueChange = {},
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Numbers,
+                                    contentDescription = "",
+                                    tint = Color(0xFF9C27B0)
+                                )
+                            },
+                            label = {
+                                Text(
+                                    text = stringResource(R.string.age)
+                                )
+                            },
+                            shape = RoundedCornerShape(16.dp)
+                        )
+                        OutlinedTextField(
+                            value = "",
+                            onValueChange = {},
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 8.dp),
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Balance,
+                                    contentDescription = "",
+                                    tint = Color(0xFF9C27B0)
+                                )
+                            },
+                            label = {
+                                Text(
+                                    text = stringResource(R.string.weight)
+                                )
+                            },
+                            shape = RoundedCornerShape(16.dp)
+                        )
+                        OutlinedTextField(
+                            value = "",
+                            onValueChange = {},
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Height,
+                                    contentDescription = "",
+                                    tint = Color(0xFF9C27B0)
+                                )
+                            },
+                            label = {
+                                Text(
+                                    text = stringResource(R.string.height)
+                                )
+                            },
+                            shape = RoundedCornerShape(16.dp)
+                        )
+
+                    }
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(48.dp),
+                        shape = RoundedCornerShape(8.dp)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.calculate),
+                            fontSize = 20.sp
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun UserDataScreenPreview() {
+    UserDataScreen()
+}
